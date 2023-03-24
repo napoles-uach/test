1509b7905a4b3d9c9e5c373b901f194f7917d7f5
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2023-03-23 20:14:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ from google.oauth2 import service_account
 from google.cloud import firestore
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
+st.write('Hell')
