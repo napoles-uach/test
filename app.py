c819c00133868c81561bdd3c69418cd29d54aeba
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2023-03-23 20:03:50 -0600

    auth

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
-
-st.title('Hello :+1:!!')
-
+import json
+from google.oauth2 import service_account
+from google.cloud import firestore
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
